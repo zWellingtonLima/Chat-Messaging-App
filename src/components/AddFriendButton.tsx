191f86a54bb6e5b08852d3defcd0f8a8ee792c29
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-05-04 21:07:42 +0100

    Initiated form validation process.

---
+++ src/components/AddFriendButton.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { FC, useState } from "react";
+import Button from "./ui/Button";
+import axios, { AxiosError } from 'axios'
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import { z } from 'zod'
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+
+interface AddFriendButtonProps {}
+
+const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+  const [showSuccessState, setShowSuccessState] = useState<boolean>(false)
+
+  const {} = useForm<FormData>({
+    resolver: zodResolver(addFriendValidator),
+  })
+
+  const addFriend = async (email: string) => {
+    try {
+      const validatedEmail = addFriendValidator.parse({ email })
+
+      await axios.post('/api/friends/add', {
+        email: validatedEmail,
+      })
+
+      setShowSuccessState(true)
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return        
+      }
+
+      if (error instanceof AxiosError) {
+        return
+      }
+    }
+  }
+
+  return (
+    <form className="max-w-sm">
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Add friend by E-Mail
+      </label>
+
+      <div className="mt-2 flex gap-4">
+        <input
+          type="text"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="you@example.com"
+        />
+        <Button>Add</Button>
+      </div>
+    </form>
+  );
+};
+
+export default AddFriendButton;
