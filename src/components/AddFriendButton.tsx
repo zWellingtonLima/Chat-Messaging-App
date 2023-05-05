f293ed309b8fc7d55765eb7a681e7e748c2b054d
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-05-05 20:20:12 +0100

    type Formdata added.

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -10,10 +10,14 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 interface AddFriendButtonProps {}
 
+type FormData = z.infer<typeof addFriendValidator>
+
 const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
   const [showSuccessState, setShowSuccessState] = useState<boolean>(false)
 
-  const {} = useForm<FormData>({
+  const {
+    register, handleSubmit
+  } = useForm<FormData>({
     resolver: zodResolver(addFriendValidator),
   })
 
@@ -48,6 +52,7 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
 
       <div className="mt-2 flex gap-4">
         <input
+          {...register('email')}
           type="text"
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="you@example.com"
